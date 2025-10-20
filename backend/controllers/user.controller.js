bc279ed870208bd40f6e1e1b29a238e4682b0b5a
Satyam8589 <satyamkumarsingh9898@gmail.com>
2025-10-20 08:47:38 +0530

    User controller updated

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,137 +3,252 @@ import User from "../models/user.model.js";
 import Profile from "../models/profile.model.js";
 import crypto from "crypto";
 import { json } from "stream/consumers";
+import { profile } from "console";
+import PDFDocument from "pdfkit";
+import connectionRequest from "../models/connections.model.js"
+import fs from "fs";
+
+const convertUserDataToPDF = async (userData) => {
+  const doc = new PDFDocument();
+
+  const outputPath = crypto.randomBytes(32).toString("hex") + ".pdf";
+  const stream = fs.createWriteStream("uploads/" + outputPath);
+
+  doc.pipe(stream);
+
+  doc.image(`uploads/${userData.userId.profilePicture}`, {
+    align: "center",
+    width: 100,
+  });
+  doc.fontSize(14).text(`Name: ${userData.userId.name}`);
+  doc.fontSize(14).text(`Username: ${userData.userId.username}`);
+  doc.fontSize(14).text(`Email: ${userData.userId.email}`);
+  doc.fontSize(14).text(`Bio: ${userData.bio}`);
+  doc.fontSize(14).text(`Current Position: ${userData.currentPosition}`);
+
+  doc.fontSize(14).text("Past Work: ");
+  userData.pastWork.forEach((work, index) => {
+    doc.fontSize(14).text(`Company Name: ${work.companyName}`);
+    doc.fontSize(14).text(`Position: ${work.position}`);
+    doc.fontSize(14).text(`Years: ${work.years}`);
+  });
+
+  doc.end();
+
+  return outputPath;
+};
 
 export const register = async (req, res) => {
-    try {
+  try {
+    const { name, email, password, username } = req.body;
 
-        const { name, email, password, username } = req.body;
+    if (!name || !email || !password || !username)
+      return res.status(400).json({ message: "All fields are required" });
 
-        if ( !name || !email || !password || !username ) return res.status(400).json({message: "All fields are required" });
-        
-        const user = await User.findOne({
-            email,
-            username
-        });
+    const user = await User.findOne({
+      email,
+      username,
+    });
 
-        if (user) return res.status(400).json({ message: "User already exists" });
+    if (user) return res.status(400).json({ message: "User already exists" });
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const newUser = new User({
-            name,
-            email,
-            password: hashedPassword,
-            username
-        });
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+      username,
+    });
 
-        await newUser.save();
+    await newUser.save();
 
-        const profile = new Profile({
-            userId: newUser._id
-        });
+    const profile = new Profile({
+      userId: newUser._id,
+    });
 
-        await profile.save();
+    await profile.save();
 
-        console.log(newUser);
+    console.log(newUser);
 
-        return res.json({ message: "User created successfuly" });
-
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
-    }
+    return res.json({ message: "User created successfuly" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const login = async (req, res) => {
-    
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        if ( !email || !password) return res.status(400).json({ message: "All fields are required" });
+    if (!email || !password)
+      return res.status(400).json({ message: "All fields are required" });
 
-        const user = await User.findOne({
-            email
-        })
+    const user = await User.findOne({
+      email,
+    });
 
-        if (!user) return res.status(404).json({ message: "User not exists" });
+    if (!user) return res.status(404).json({ message: "User not exists" });
 
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
 
-        const token = crypto.randomBytes(32).toString("hex");
+    const token = crypto.randomBytes(32).toString("hex");
 
-        await User.updateOne({ _id: user._id }, { token });
+    await User.updateOne({ _id: user._id }, { token });
 
-        return res.json({ message: "You are successfully login",  token });
-
-    } catch (error) {
-        return res.status(500).json({ message: error.message }); 
-    }
+    return res.json({ message: "You are successfully login", token });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const uploadProfilePicture = async (req, res) => {
-    const { token } = req.body;
+  const { token } = req.body;
 
-    try {
-        
-        const user = await User.findOne({ token: token});
-        if (!user) return res.status(400).json({ message: "User not exists" });
+  try {
+    const user = await User.findOne({ token: token });
+    if (!user) return res.status(400).json({ message: "User not exists" });
 
-        user.profilePicture = req.file.filename;
+    user.profilePicture = req.file.filename;
 
-        await user.save();
+    await user.save();
 
-        return res.json({ message: 'Profile picture updated' });
-
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
-    }
+    return res.json({ message: "Profile picture updated" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const updateUserProfile = async (req, res) => {
+  try {
+    const { token, ...newUserData } = req.body;
 
-    try {
-        const { token, ...newUserData } = req.body;
+    const user = await User.findOne({ token: token });
 
-        const user = await User.findOne({ token: token });
+    if (!user) return res.status(404).json({ message: "User not found" });
 
-        if (!user) return res.status(404).json({ message: "User not found" });
+    const { username, email } = newUserData;
 
-        const { username, email } = newUserData;
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
 
-        const existingUser = await User.findOne({ $or: [{ username }, { email }]});
-
-        if (existingUser) {
-            if (existingUser || String(existingUser._id) !== String(user._id)) {
-                return res.status(400).json({ message: "User already exists" });
-            }
-        } 
-
-        Object.assign(user, newUserData);
-        await user.save();
-
-        return res.json({ message: "User updated" });
-
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
+    if (existingUser) {
+      if (existingUser || String(existingUser._id) !== String(user._id)) {
+        return res.status(400).json({ message: "User already exists" });
+      }
     }
+
+    Object.assign(user, newUserData);
+    await user.save();
+
+    return res.json({ message: "User updated" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const getUserAndProfile = async (req, res) => {
-    
-    try {
-        const { token } =  req.body;
+  try {
+    const { token } = req.body;
 
-        const user = await User.findOne({ token: token });
+    const user = await User.findOne({ token: token });
 
-        if (!user) return res.status(400).json({ message: "User not found" });
+    if (!user) return res.status(400).json({ message: "User not found" });
 
-        const userProfile = await Profile.findOne({ userId: user._id })
-            .populate("userId", "name email username profilePicture");
+    const userProfile = await Profile.findOne({ userId: user._id }).populate(
+      "userId",
+      "name email username profilePicture"
+    );
 
-        return res.json(userProfile);
-        
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
-    }
+    return res.json(userProfile);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateProfileData = async (req, res) => {
+  try {
+    const { token, ...newProfileData } = req.body;
+    const userProfile = await User.findOne({ token: token });
+
+    if (!userProfile)
+      return res.status(404).json({ message: "User not found" });
+
+    const profile_to_update = await Profile.findOne({
+      userId: userProfile._id,
+    });
+
+    Object.assign(profile_to_update, newProfileData);
+
+    await profile_to_update.save();
+
+    return res.json({ message: "User profile updated" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const getAllUserProfile = async (req, res) => {
+  try {
+    const profile = await Profile.find().populate(
+      "userId",
+      "name username email profilePicture"
+    );
+
+    return res.json({ profile });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const downloadProfile = async (req, res) => {
+  const user_id = req.query.id;
+
+  const userProfile = await Profile.findOne({ userId: user_id }).populate(
+    "userId",
+    "name username email profilePicture"
+  );
+
+  let outputPath = await convertUserDataToPDF(userProfile);
+
+  return res.json({ message: outputPath });
+};
+
+export const sendConnectionRequest = async (req, res) => {
+
+  const { token, connectionId } = req.body;
+
+  try {
+    const user = await User.findOne({ token });
+
+    if(!user) return res.status(404).json({ message: "User not found" });
+
+    const connectionUser = await User.findOne({ _id: connectionId });
+
+    if(!connectionUser) return res.status(404).json({ message: "Connection user not found" });
+
+    const existingRequest = await connectionRequest.findOne(
+      {
+        userId: user._id,
+        connectionId: connectionUser._id
+      }
+    )
+
+    if(!existingRequest) return res.status(400).json({ message: "Request already sent" });
+
+    const request = new connectionRequest(
+      {
+        userId: user._id,
+        connectionId: connectionUser._id
+      }
+    )
+
+    await request.save();
+
+    return res.json({ message: "Request sent" });
+
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getUserAndProfile, login, register, updateUserProfile, uploadProfilePicture } from "../controllers/user.controller.js";
+import { downloadProfile, getAllUserProfile, getUserAndProfile, login, register, updateProfileData, updateUserProfile, uploadProfilePicture } from "../controllers/user.controller.js";
 import multer from "multer";
 
 
@@ -22,5 +22,8 @@ router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/user_update").post(updateUserProfile);
 router.route("/get_user_and_profile").get(getUserAndProfile);
+router.route("/update_profile_data").post(updateProfileData);
+router.route("/user/get_all_users").get(getAllUserProfile);
+router.route("/user/download_resume").get(downloadProfile);
 
 export default router;
