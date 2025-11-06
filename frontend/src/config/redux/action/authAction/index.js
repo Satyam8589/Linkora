e452a03151ed0fc2c893004acca208ef7a9fd6e9
Satyam8589 <satyamkumarsingh9898@gmail.com>
2025-11-06 10:26:38 +0530

    redux initialize

---
+++ frontend/src/config/redux/action/authAction/index.js
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+
+export const loginUser = createAsyncThunk(
+  'auth/loginUser',
+  async (user, thunkAPI) => {
+    try {
+      const response = await clientServer.post('/login', {
+        email: user.email,
+        password: user.password,
+      });
+
+      if (response.data.token) {
+        localStorage.setItem("token", response.data.token);
+      } else {
+        return thunkAPI.rejectWithValue({
+          message: "token not provided"
+        });
+      }
+
+      return thunkAPI.fulfillWithValue(response.data.token);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
