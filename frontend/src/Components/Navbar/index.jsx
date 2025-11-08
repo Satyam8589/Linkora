dab8f7613fcd215740945e96e1c7cd6986b3a165
Satyam8589 <satyamkumarsingh9898@gmail.com>
2025-11-08 14:44:51 +0530

    home page and navbar initial set up

---
+++ frontend/src/Components/Navbar/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from "./styles.module.css"
+import { useRouter } from 'next/router';
+
+function NavBarComponent() {
+
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <nav className={styles.navBar}>
+        <h2 style={{cursor: "pointer"}} onClick={() => {
+          router.push("/")
+        }}>Pro Connect</h2>
+
+        <div className={styles.navBarOptionContainer}>
+      <div
+        onClick={() => {
+          router.push("/login");
+        }}
+        className={styles.buttonJoin}
+      >
+        <p>Be a part</p>
+      </div>
+    </div>
+      </nav>
+
+    </div>
+  )
+}
+
+export default NavBarComponent
---
+++ frontend/src/pages/login/index.jsx
@@ -0,0 +1,12 @@
+import UserLayout from "@/layout/UserLayout";
+import React from "react";
+
+function LoginComponent() {
+  return (
+    <UserLayout>
+      <h2>Login page</h2>
+    </UserLayout>
+  );
+}
+
+export default LoginComponent;
